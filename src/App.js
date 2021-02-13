3c4969f6670b0c045f623d104f21fc80ec7a79b0
SRHIR Abdeljalil <s.abdeljalil2014@gmail.com>
2021-02-13 15:45:07 +0100

    fetch Ok + defins .env )

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    dataVelib: [],
+  };
+
+  //constructor(props) {
+  //  super(props);
+  //this.state = {
+  //  dataVelib: {},
+  // };
+  // }
+
+  componentDidMount() {
+    console.log("Fetching de API = " + process.env.REACT_APP_BASE_URL);
+
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    axios
+      .get(process.env.REACT_APP_BASE_URL)
+      .then((response) => {
+        this.setState({ dataVelib: response.data.records });
+        console.log(this.state.dataVelib);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.datasetid}</h1>
+
+          <ul>
+            {this.state.dataVelib.map((item) => (
+              <li key={item.datasetid}>{item.datasetid}</li>
+            ))}
+          </ul>
+
+          <button className="button" onClick={this.fetchData}>
+            <span>UPDATE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
